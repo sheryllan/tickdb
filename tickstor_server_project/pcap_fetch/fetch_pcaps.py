99ac3ce73f5f037aaf9d6e83b3aee0361ac23ab4
ognen bezanov <ognen.bezanov@liquidcapital.com>
2014-08-07 13:12:43 +0100

    Bugfixes

--- tickstor_server_project/pcap_fetch/fetch_pcaps.py
+++ tickstor_server_project/pcap_fetch/fetch_pcaps.py
@@ -41,7 +41,7 @@ def bunzip2dir(directory):
         map(lambda x: perr(x,ERRLOG), failedunzip)
         return failedunzip
     else:
-        return True #all OK
+        return []#all OK
 
 def pullPCAP(host,path, attempts=4):
     targetpath = "%s/%s/%s" % (scratchpath, host, ts)
@@ -59,40 +59,5 @@ def pullPCAP(host,path, attempts=4):
 
     perr("ERROR: Failed rsync! from %s:%s to %s" % (host,path,targetpath),ERRLOG)
     return (False, None) #we failed to do the rsync
-
-if __name__ == "__main__":
-    failed_transfers = pflexidict()
-    intray = sources 
-
-    for row in intray:
-        # 1. Get the data from the remote host
-#        result, savedpath = pullPCAP(row[0],row[1])
-        result = True
-        savedpath = "/storage/scratchdisk/lcmfrs23/1407332608/"
-
-        if result == False: 
-            perr("ERROR: We failed rsync transfer! >> %s:%s" % (row[0],row[1]) )
-            row.append("ERROR: rsync failed")
-            failed_transfers[row[0]] = row
-            continue
-
-        # 2. Make sure we have the two folders there that we need.
-        if len(filter(lambda x: not os.path.exists(os.path.join(savedpath,x)), target_pcap_folders )) != 0:
-            perr("ERROR: EMDI and/or ETI folders are missing from '%s'" % savedpath,ERRLOG)
-            row.append("ERROR: EMDI/ETI folders are missing from '%s'" % savedpath)
-            failed_transfers[row[0]] = row
-            continue
-
-        #3. All folders are there. bunzip them all!
-        failures = bunzip2dir(savedpath)
-        if len(failures) > 0:
-            map(lambda x: perr(x,ERRLOG), failures)
-            perr("ERROR: Could not bunzip all files. See log for more errors. ",ERRLOG)
-            row.append("ERROR: Could not bunzip all files. See log for more errors. ", savedpath )
-            failed_transfers[row[0]] = row
-            continue
-
-
-
 
 
