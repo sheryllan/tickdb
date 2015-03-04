1570aeedba5cc07691c9df24c58d8f869bf85c32
David Bellot <dbellot@liquidcapital.com>
2015-03-04 12:25:22 +0000

    added bzip2 compressor

--- database_builder/database_builder.py
+++ database_builder/database_builder.py
@@ -3,7 +3,7 @@
 # import modules
 import time,os,argparse,sys
 import csv
-import gzip,lzma
+import gzip,lzma,bz2
 import mimetypes as mim
 from datetime import *
 from decimal import *
@@ -21,9 +21,8 @@ def main():
 	parser.add_argument("--levels", "-l", help="max OB levels", type=int,default=5)
 	parser.add_argument("--odir",   "-o", help="output directory",type=str,default=".")
 	parser.add_argument("--decoder","-d", help="decoder type: qtg_eobi,qtg_emdi,pcap_eobi,pcap_emdi",type=str,default="qtg_eobi")
-	parser.add_argument("--format", "-f", help="output format: TOB,...",type=str,default=".")
-	parser.add_argument("--ofmt",   "-m", help="output file format: csv, csv.xz, hdf,...",type=str,default="csv.xz")
-	parser.add_argument("--insdb",  "-i", help="Instrument database",type=str,default="/data/database/instRefdataCoh.csv")
+	parser.add_argument("--format",   "-f", help="output file format: csv, csv.bz2, csv.xz, hdf,...",type=str,default="csv")
+	parser.add_argument("--insdb",  "-i", help="Instrument database",type=str,default="/mnt/data/database/instRefdataCoh.csv")
 	parser.add_argument("--log",    "-g", help="loggin file",type=str,default="./obp.log")
 	parser.add_argument("ifname",help="input filename",type=str)
 	args = parser.parse_args()
@@ -57,12 +56,17 @@ def main():
 	for uid in books:
 		x = DataFrame(books[uid].output,columns=books[uid].header)
 		prodname = df[df['#instuid']==uid].iat[0,3]
-		filename = args.odir+"/"+prodname+"_"+"20"+date+"."+args.ofmt
-		if args.ofmt=="csv":
-			x.to_csv(filename,na_rep="NA",index=False,index_label=False)
-		elif args.ofmt=="csv.xz":
-			xzfile = lzma.open(filename,"wt",format=lzma.FORMAT_XZ,preset=9|lzma.PRESET_EXTREME)
-			x.to_csv(xzfile,na_rep="NA",index=False,index_label=False)
+		output_file = args.odir+"/"+prodname+"_"+"20"+date+"."+args.format
+
+		if args.format=="csv":
+			pass
+		elif args.format=="csv.xz":
+			output_file = lzma.open(output_file,"wt",format=lzma.FORMAT_XZ,
+					preset=9|lzma.PRESET_EXTREME)
+		elif args.format=="csv.bz2":
+			output_file = bz2.open(output_file,"wt",9,"UTF-8")
+
+		x.to_csv(output_file,na_rep="NA",index=False,index_label=False)
 
 	sys.exit(0)
 
