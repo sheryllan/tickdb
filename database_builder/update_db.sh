910d7a985d58920913b9e1e2206e1e2b73e84638
David Bellot <dbellot@liquidcapital.com>
2015-04-01 15:00:26 +0100

    RCTR-23454 added unwanted files to the process. Reduced to 10 jobs max

--- database_builder/update_db.sh
+++ database_builder/update_db.sh
@@ -28,14 +28,37 @@ function is_database_builder_in_path {
 which database_builder.py &> /dev/null ; echo $?
 }
 
+function symbol_per_day {
+/bin/env python3 -c '
+import sys
+x={}
+
+for line in sys.stdin:
+	line=line.split()
+	symbol=line[0]
+	date=line[1]
+
+	if date not in x:
+		x[date]=list()
+	x[date].append(symbol)
+
+dates=sorted(x.keys())
+for d in dates:
+	print("{0},".format(d),end="")
+	print(",".join(sorted(x[d])))
+'
+}
+
 # ---------------------------
 #    Setup main parameters
 # ---------------------------
-rootdir=/mnt/data/
+rootdir=/mnt/data
 tmpdir=/tmp
 level=5
 dbdir=${rootdir}/database
 dbprocessed=${dbdir}/processed_files.db
+unwanted=${dbdir}/unwanted_files.db
+symbol_per_day=${dbdir}/symbol_per_day.db
 timestamp=$(date --utc --rfc-3339='ns' | tr ' .:+-' '_')
 gnupar=$(which parallel)
 qtg_src_dir=${rootdir}/qtg
@@ -43,12 +66,17 @@ parjobfile=/tmp/gnupar_job_file.sh
 nbcores=$(($(nproc)-2)) # get all the cores minus 2
 
 # Check if db file exists
-if [ ! -f ${dbprocessed} ]; then
+if [ ! -e ${dbprocessed} ]; then
 	touch ${dbprocessed}
 fi
 
+# Check if the unwanted database exists
+if [ ! -e ${unwanted} ]; then
+	unwanted=""
+fi
+
 # Check if GNU parallel exists
-if [ ! -f ${gnupar} ]; then
+if [ ! -e ${gnupar} ]; then
 	echo "Error: GNU parallel not found"
 	exit 1
 fi
@@ -72,19 +100,42 @@ new_qtg=${tmpdir}/new_qtg_${timestamp}
 find ${qtg_src_dir} -name '*.dat.gz' -type f > ${all_qtg}
 
 # find new files only
-sort ${all_qtg} ${dbprocessed} | uniq -u | sort - ${all_qtg} | uniq -d > ${new_qtg}
+sort ${all_qtg} ${dbprocessed} ${unwanted} | uniq -u > ${new_qtg}
 rm -f ${all_qtg}
 
 rm -f ${parjobfile}
 while read line
 do
-	outputdir = ${dbdir}/qtg/$(exchange ${line})/$(month ${line})
+	outputdir=${dbdir}/qtg/$(exchange ${line})/$(month ${line})
 	mkdir -p ${outputdir} # -p create dirs all the way long and does not fail if dir exists
-	echo -n ${database_builder} -l 5 -o ${outputdir} -d $(decoder ${line}) -f csv.bz2 -i ${dbdir}/qtg/instRefdataCoh.csv -g ${dbdir}/qtg/qtg.log ${line} >> ${parjobfile}
+	echo ${database_builder} -l 5 -o ${outputdir} -d $(decoder ${line}) -f csv.bz2 -i ${dbdir}/qtg/instRefdataCoh.csv -g ${dbdir}/qtg/qtg.log ${line} >> ${parjobfile}
 done < "${new_qtg}"
 
-cat ${parjobfile} | ${gnupar} -j ${nbcores} &> /dev/null
+#cat ${parjobfile} | ${gnupar} -j ${nbcores} &> /dev/null
+cat ${parjobfile} | ${gnupar} -j 10 &> /dev/null
 
 rm -f ${parjobfile} 
 cat ${new_qtg} >> ${dbprocessed}
 rm -f ${new_qtg}
+
+# -----------------------
+#    LIQUID PCAP FILES
+# -----------------------
+
+# ----------------------------
+#    STATISTICS AND REPORTS
+# ----------------------------
+
+# Find available days for each symbol in QTG database
+
+# find produced files | extract symbol name and date in 2 columns |
+# remove _ | create a csv files with date,symbol,symbol,symbol,...
+# for each date we obtain the list of available symbols in the database
+
+find ${dbdir} -name '*.bz2' |\
+${gnupar} basename {} .csv.bz2 |\
+awk -F '_' '{print $1,$2}' |\
+symbol_per_day
+
+
+# Run daily statistic on each symbol
