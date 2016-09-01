01c9793ea347b59f01955b84d2bc9bf0f416ed7b
David Bellot <dbellot@liquidcapital.com>
2016-09-01 16:50:44 +0100

    done with new liquid capture builder

--- database_builder/liquid_capture_update_db.py
+++ database_builder/liquid_capture_update_db.py
@@ -1,228 +1,233 @@
 #!/usr/bin/env python3.5
 
-# IMPORTANT: this code is only a translation of the previous code written in bash and awk
-# As the project is becoming more complex, I implemented it again in Python.
-# That's why the structure and the way I manipulate strings can be strange sometimes
-# More improvement is needed to make it more python-integrated.
-# - David 18-03-2016
+import time,os,argparse,sys,json,re,subprocess,csv,fnmatch,pandas,lzma,re,io
+import datetime
+from joblib import Parallel, delayed
+import pwd,grp
 
-import time,os,argparse,sys
-import json
-import re
-import subprocess
-import csv
+""" This program generates and updates the TickDB from the data captured by the Liquid servers
+The design has been simplified at the cost of reading directories recursively. It is however
+more resilient than the previous version and can fix more problems when the data are corrupted
+so that the user doesn't have to intervene (and the user is me.... !)
 
-# clean up bad characters in Reference files
-def filtchar(x):
-	a=bytearray(range(0,256))
-	b=bytearray(b'xxxxxxxxxx\nxxxxxxxxxxxxxxxxxxxxx !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7fxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-	t=bytes.maketrans(a,b)
-	return x.translate(t)
+So I traded off speed for simplicity. But given the speed of the server itself, it doesn't make
+a bit difference in CPU and workload.
+"""
 
-def merge_first_fields(l,hdrsize):
-	d = len(l)-hdrsize # how many fields to merge at the front of the list
-	# merge and add the rest of the list. LISP ??? :-D
-	return [",".join(l[0:d+1])]+l[d+1:len(l)]
+def ensure_i_am_alone():
+    """ prevent running 2 instances of the update_db program """
+    pid = str(os.getpid())
+    pidfile = "/tmp/mypid.pid"
+    while os.path.exists(pidfile):
+        time.sleep(20)
+    pidfile = open("/tmp/mypid.pid",'w')
+    pidfile.write(pid)
 
-def process_ref_data(rawdata_dir,ref_data_file):
-	# find all ref data files in the raw data repository
-	ref_files=[]
-	for root,dirs,files in os.walk(rawdata_dir):
-		for f in files:
-			if f.endswith(".csv") and 'ReferenceData' in f:
-				ref_files.append(os.path.join(root,f))
+def run_compression(f): # run compression job on file f
+    os.system('xz -9 '+f)
 
-	# read all the files
-	ref_data={}
-	hdrsize = 0
-	for f in ref_files:
-		with open(f,'rb') as rf:
-			# read entire file
-			for line in rf:
-				# remove white spaces first
-				line = line.replace(b' ',b'')
-				# remove bad chars, convert to str, split to csv elements
-				l = filtchar(line).decode('utf-8').rstrip(',\n').split(',')
-				# get header size
-				if l[0]=='ProductID':
-					hdrsize=len(l)
-				# some lines have a product ID with commas
-				if hdrsize>0 and len(l)>hdrsize:
-					l = merge_first_fields(l,hdrsize)
-				# get Product Id (sometimes ProductID have commas in their name)
-				ref_data[l[0]] = l # store line, replacing each elmt with its last version
+def compression_job(config_file):
+    """ Wait for all new files to be copied, then compress them with xz """
+    def find_files(rawdata_dir): # find all files in the raw data repository
+        raw_files=[]
+        for root,dirs,files in os.walk(rawdata_dir):
+            for f in files:
+                if f.endswith(".csv"): # Reactor writes uncompressed csv files
+                    raw_files.append(os.path.join(root,f))
+        return raw_files
 
-	return ref_files
-	
-	# write output to CSV file
-	with open(ref_data_file,'w') as f:
-		writer=csv.writer(f)
-		writer.writerow(ref_data['ProductID']) # write header
-		del ref_data['ProductID']
-		writer.writerows([ref_data[k] for k in ref_data]) # dump the rest of the file
+    # Dummy way to see if the files can be compressed: check if their size is not growing anymore
+    # for x seconds. The writer always finishes its job at some point. When all the files are
+    # ready the function returns
+    def monitor_raw_files(raw_files,delay=5):
+        fsize={}
+        no_growing_files = 0
+        for f in raw_files: # get initial sizes
+            fsize[f] = os.stat(f).st_size
 
-def find_files(rawdata_dir,processed_files):
-	# read list of already processed files
-	proc_files = open(processed_files,'r').readlines()
-	proc_files = [x[:-1] for x in proc_files]
+        while no_growing_files < len(raw_files):
+            time.sleep(delay)
+            for f in raw_files:
+                s = os.stat(f).st_size
+                if fsize[f] != s:
+                    fsize[f] = s
+                else:
+                    no_growing_files = no_growing_files + 1
 
-	# find all files in the raw data repository
-	raw_files=[]
-	for root,dirs,files in os.walk(rawdata_dir):
-		for f in files:
-			if f.endswith(".csv"): # Reactor writes uncompressed csv files
-				raw_files.append(os.path.join(root,f))
+    # run compression job
+    if not os.path.isfile(config_file):
+        return False
 
-	# extract the new files which have never been processed before
-	return [x for x in set(raw_files).difference(set(proc_files)) if 'ReferenceData' not in x]
+    with open(config_file) as cfg_file:
+        cfg=json.load(cfg_file) # get json config file
+        rawdata_dir = cfg['liquid_capture']['src_dir']
+        new_files = find_files(rawdata_dir) # get list of .csv files
+        monitor_raw_files(new_files,8) # check all files are uploaded
+        print("Compressing ",len(new_files)," files")
+        results = Parallel(n_jobs=-1)(delayed(run_compression)(f) for f in new_files)# run // jobs
 
-def create_month_directories(dbdir,rawfiles):
-	# extract months from filenames
-	months = [ os.path.basename(os.path.dirname(x))[:-2] for x in rawfiles ]
+def copy_liquid_capture_to_db(config_file):
+    """ Find new files from the raw data of liquid_capture and process them into the DB """
+    def find_compressed_files(rawdata_dir):
+        """ get all the raw data compressed files """
+        raw_files=[]
+        for root,dirs,files in os.walk(rawdata_dir):
+            for f in files:
+                if f.endswith(".csv.xz") and 'Reference' not in f:
+                    raw_files.append(os.path.join(root,f))
+        return raw_files
 
-	# remove duplicates
-	months = list(set(months))
+    def find_db_files(dbdir):
+        """ get all the files from the database """
+        db_files = []
+        for root,dirs,files in os.walk(dbdir):
+            for f in files:
+                if f.endswith(".csv.bz2"):
+                    db_files.append(os.path.join(root,f))
+        return db_files
 
-	# create non existing directories
-	for m in months:
-		d = os.path.join(dbdir,m)
-		if not os.path.exists(d):
-			os.makedirs(d)
+    def create_dict_of_files(raw_files,db_files,prefix,dbdir):
+        """ create a dictionary with key = target db file and value =
+        list of raw data files to process """
+        D = {} # build dict of new files to process
+        for f in raw_files:
+            x=os.path.basename(f).split('.')[0] # remove extensions
+            for p in prefix: # remove any "exchange" prefix
+                x=x.replace(p,'')
+            ts=x.split('-')[-1]
+            month= str(int(x.split('-')[-2])//100) # get month
+            new_file=dbdir+'/'+month+'/'+x
+            new_file=new_file.replace('-'+ts,'.csv.bz2')
+            if new_file not in db_files: # check in file exists in db
+                if new_file not in D: # if not, add the raw file to the list
+                    D[new_file] = [f] # of files to process (because the same
+                else: # instrument can have many files)
+                    D[new_file].append(f)
 
-# return a dict of prefix file names as a key and a list of timestamps for each complete file name
-# because some products have several capture files per day and we want to concat them into
-# one final file per day
-def create_dict_of_files(rawfiles):
-	D = {}
-	for f in rawfiles:
-		# decompose file name to extract the prefix only
-		# the timestamp is always the last field
-		# example: <some exchange/appliance>-A50-F-APR2016-20160317-072908.csv ->
-		x = os.path.basename(f).split('.')[0].split('-') # remove .csv and decompose
-		timestamp = int(x[len(x)-1]) # extract timestamp
-		x.pop() # remove timestamp
-		short_name = os.path.join(os.path.dirname(f),'-'.join(x)) # recompose filename
+        for k in D: # sort each sub list to be sure we concat files chronologically
+            D[k].sort()
 
-		# Store the result in a tuple
-		if short_name not in D:
-			D[short_name] = [timestamp]
-		else:
-			D[short_name].append(timestamp)
+        return D
 
-	# sort each sub list to be sure we concat files chronologically
-	for k in D:
-		D[k].sort()
-	
-	return D
+    def create_recompress_job_list(D):
+        """ make a list of bash command to recompress everything """
+        cmd=[]
+        for k in D:
+            s="( xz -cd "+D[k].pop(0)+";" # first file of the list
+            for x in D[k]:
+                s=s+"xz -cd "+x+"|tail -n +2;" # remove first line of other files
+            s=s+") | bzip2 -9c > " + k
+            cmd.append(s)
 
-def usage():
-	print("Usage: ./liquid_capture_update_db.py <json config file>",file=sys.stderr)
+        return cmd
 
-def main(argv):
-	if(len(argv)<2):
-		usage()
-		sys.exit(1)
-	config = argv[1]
+    def create_missing_dirs(D,uid,gid):
+        """ create missing directories for target files """
+        for d in list(set([os.path.dirname(k) for k in D])): # extract unique target dir names
+            if not os.path.exists(d):
+                os.makedirs(d,mode=0o750) # create dir with mode 750
+                os.chown(d,uid,gid) # change owner and group
 
-	# Test json conf file first
-	if not os.path.isfile(config):
-		print("Error: json config file",config," does not exist",file=sys.stderr)
-		sys.exit(1)
+    # main program of this function
+    with open(config_file) as cfg_file:
+        cfg=json.load(cfg_file) # get json config file
+        rawdata_dir = cfg['liquid_capture']['src_dir']
+        dbdir =       cfg['liquid_capture']['dbdir']
+        prefix =      cfg['liquid_capture']['prefix']
+        owner =       cfg['liquid_capture']['owner']
+        group =       cfg['liquid_capture']['group']
 
-	# prevent running 2 instances of the update_db program
-	pid = str(os.getpid())
-	pidfile = "/tmp/mypid.pid"
-	while os.path.exists(pidfile):
-		time.sleep(20)
-	pidfile = open("/tmp/mypid.pid",'w')
-	pidfile.write(pid)
+        uid = pwd.getpwnam(owner).pw_uid
+        gid = grp.getgrnam(group).gr_gid
 
-	try:
-		with open(config) as jsonfile:
-			# Read json config
-			cfg = json.load(jsonfile)
-			rawdata_dir = cfg['liquid_capture']['src_dir']
-			processed_files = cfg['liquid_capture']['dbprocessed']
-			dbdir = cfg['liquid_capture']['dbdir']
-			ref_data_file = cfg['liquid_capture']['instdb']
-			prefix = cfg['liquid_capture']['prefix']
-			tmpdir = cfg['tmpdir']
-			nbcores = cfg['nbcores']
-			gnupar = os.popen("which parallel").read().strip()
+        raw_files = find_compressed_files(rawdata_dir) # find all files in raw data
+        db_files = find_db_files(dbdir) # find all files in DB
+        D = create_dict_of_files(raw_files,db_files,prefix,dbdir) # make a diff a generate new files to process
+        job = create_recompress_job_list(D) # make a shell script
+        create_missing_dirs(D,uid,gid)
+        print("Running ",len(job)," jobs")
+        results = Parallel(n_jobs=-1)(delayed(os.system)(f) for f in job)# run // jobs
+        for k in D: # finally change owners of each new file
+            os.chown(k,uid,gid)
 
-			# Check if GNU parallel exists
-			if gnupar=='':
-				print("Error: GNU parallel not found",file=sys.stderr)
-				sys.exit(1)
+def generate_reference_data(config_file):
+    """ generate reference data main file from all the reference files """
+    def find_files(rawdata_dir): # find all files in the raw data repository
+        raw_files=[]
+        for root,dirs,files in os.walk(rawdata_dir):
+            for f in files:
+                if fnmatch.fnmatch(f,'*Ref*'): # look for Reference data files
+                    raw_files.append(os.path.join(root,f))
+        return raw_files
 
-			# Check if database file of processed files exists
-			if not os.path.exists(processed_files):
-				open(processed_files,'w').close() # create empty file
+    def load_and_merge_ref_files(raw_files, target_file):
+        header=['ProductID','Product','Type','Exchange','Currency','Underlying','ExpiryDate',
+                'Strike','PutOrCall','ExerciseStyle','MinPriceIncrement','MinPriceIncrementAmount',
+                'SecurityDesc','PremiumDecimalPlace','SecurityID','UnderlyingSecurityID',
+                'MarketSegmentID','MarketSegment','DestinationExchange']
+        decopt = re.compile("([0-9]),([0-9]+\\.[CP])")
+        ref = [] # resulting data.frame (as a list so far)
+        for f in raw_files:
+            # decompress the file first
+            if f.endswith('.xz'):
+                the_file = lzma.open(f,'rt',encoding='ISO-8859-1')
+            else:
+                the_file = open(f,'rt',encoding='ISO-8859-1')
 
-			# 1- retrieve file names from rawdata
-			# 2- Load PROCESSED_FILES
-			# 3- Make the difference to find new files
-			files_to_process = find_files(rawdata_dir,processed_files)
+            # read data and filter out problems
+            # filter out all sort of problems
+            l = [line.replace('\n','') for line in the_file] # read and remove all CR
+            l = [line.rstrip(',')         for line in l] # remove comma at the end of each lines
+            l = [re.sub(',",', ',,',line) for line in l] # remove a double-quote alone in between comma
 
-			# 4- Create directories if they don't exist
-			create_month_directories(dbdir,files_to_process)
-	
-			# 5- Create dict of files to process: we can have several for the same instrument
-			# in the same day (when the capture device crashes for example)
-			dfiles = create_dict_of_files(files_to_process)
-	
-			# 6- Generate job files
-			jobfile=[]
-			j=''
-			for x in dfiles:
-				if len(dfiles[x])>1:
-					j='(cat '+ x+'-'+format(dfiles[x][0],'06d')+'.csv;'
-					for i in range(1,len(dfiles[x])):
-						j=j+'tail -n +2 '+x+'-'+format(dfiles[x][i],'06d')+'.csv;'
-					j=j+')|bzip2 -c -9 '
-				else:
-					j = 'bzip2 -c -9 '+x+'-'+format(dfiles[x][0],'06d')+'.csv'
-				
-				# get month of the file
-				month = os.path.basename(os.path.dirname(x))[:-2]
-				# make a clean destination file name, removing the exchange_appliance name
-				destination = os.path.join(dbdir,month,os.path.basename(x))+'.csv.bz2'
-				for pr in prefix:
-					destination = destination.replace(pr,'')
-				j = j+' > '+ destination
-				# add job to job list
-				jobfile.append(j)
+            for c in ['\x00','\x01','\x02','\x04','\x0b','\x0c','\x10',
+                    '\x14','\x15','\x16','\x17','\x18','\x1d','\x1e','\x1f',
+                    '\xfe','`',u'\u0097',u'\u0098']:
+                l = [line.replace(c,'') for line in l] # remove bad characters
+            l = [line.replace(',&,',',,') for line in l]# remove & 
+            l = [line.replace(',NA,',',,') for line in l]# remove NA to be more generic
+            l[0] = l[0].replace(' ','') # remove spaces in header
 
-			# 7- pipe jobs to GNU parallel
-			proc = subprocess.Popen([gnupar,'-j',str(nbcores)],stdin=subprocess.PIPE)
-			proc.communicate('\n'.join(jobfile).encode())
-			proc.wait()
-  
-   			# 8- update reference data
-			ref_files = process_ref_data(rawdata_dir,ref_data_file)
-   
-	   		# 9- compress the processed files
-	   		# note: these files will not appear in the next round because we filter out
-	   		# everything but those ending with .csv
-			x=["sudo","-b","-n",gnupar,'-j',str(nbcores),'xz','-9',':::']
-			x.extend(files_to_process)
-			x.extend(ref_files)
-			proc = subprocess.Popen(x,stdin=subprocess.PIPE)
-			proc.wait()
+            # check for product name with a , in place of the decimal point for the strike
+            # if they're not surrounded by quotes, then we add them
+            for i in range(len(l)):
+                if decopt.search(l[i]):
+                    l[i]=decopt.sub('\\1_\\2',l[i])
 
-			# NOTE: the rawdata files are written by user rsprod.dev without write permission to the group dev
-			# In /etc/sudoers I added the following lines:
-			# Cmnd_Alias COMPRESSION = /usr/bin/xz, /usr/local/bin/parallel
-			# %dev ALL=NOPASSWD: COMPRESSION
-			# to allow members of the group dev (like my own user) to run sudo xz
+            # make a long string again and read it into a data.frame
+            x = pandas.read_csv(io.StringIO('\n'.join(l)))
+            # add empty missing_col
+            for missing_col in [w for w in header if w not in x.columns]:
+                x[missing_col]=pandas.Series('')
+            # reduce x to the columns we want (in header) and concat with ref
+            ref.append(x[header])
+        ref=pandas.concat(ref) # make a big data.frame
+        ref=ref.drop_duplicates() # remove duplicate lines
+        ref.to_csv(target_file,na_rep='NA',columns=header,header=header,index=False)
 
-   			# 10- update list of processed_files
-			pf = open(processed_files,'a')
-			pf.write('\n'.join(files_to_process))
-			pf.close()
- 
-	finally:
-		os.unlink("/tmp/mypid.pid")
+    # main program
+    with open(config_file) as cfg_file:
+        cfg=json.load(cfg_file) # get json config file
+        rawdata_dir = cfg['liquid_capture']['src_dir']
+        dbdir =       cfg['liquid_capture']['dbdir']
+        target_file = cfg['liquid_capture']['instdb']
+        raw_files = find_files(rawdata_dir) # find all files in raw data
+        load_and_merge_ref_files(raw_files,target_file) # create a single data.frame of all the ref data files
 
 if __name__=="__main__":
-	main(sys.argv)
+    # Get arguments or print usage
+    if(len(sys.argv)<2):
+        print(sys.argv[0]," <config file>")
+        sys.exit(1)
+    config_file = sys.argv[1]
+    # Test json conf file first
+    if not os.path.isfile(config_file):
+        print("Error: json config file",config_file," does not exist",file=sys.stderr)
+        sys.exit(1)
+
+    ensure_i_am_alone() # wait for previous scripts to finish
+    #compression_job(config_file) # Run compression jobs
+    copy_liquid_capture_to_db(config_file) # update the db with the new files
+    #generate_reference_data(config_file) # update the reference data files
+
+    os.unlink("/tmp/mypid.pid")
