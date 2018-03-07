eb57ec99a0ad0f9f404aa0cb05bd1c9582305cd3
dbellot <dbellot@liquidcapital.com>
2018-03-07 08:59:20 +0000

    bug fix

--- influx/audit.sh
+++ influx/audit.sh
@@ -14,7 +14,7 @@ awk -F ',' '{printf("%s,%s,%s,%s,%s\n",$3,$2,$1,$4,$5)}' |\
 awk -F ',' '$NF>0' > trades.csv
 
 influx -precision rfc3339 -format csv -database liquid_tick \
-	-execute "select count(price) from book group by time(1d),type,product,expiry" |\
+	-execute "select count(otype) from book group by time(1d),type,product,expiry" |\
 sed 's/"//g' |\
 grep -v '^name' |\
 sed 's/T00:00:00Z//' |\
